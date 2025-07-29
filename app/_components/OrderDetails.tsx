99d311286fcc371d9b997a14c2d59e2fd3877eea
NataliaDrause <nataliadrause@gmail.com>
2025-07-29 22:57:29 +0300

    wip order details styles

--- app/_components/OrderDetails.tsx
+++ app/_components/OrderDetails.tsx
@@ -1,28 +1,42 @@
 import { OrderFull } from '../_types/order';
+import styles from './orders.module.scss';
 
 export default function OrderDetails({ order }: { order: OrderFull }) {
   return (
-    <div>
-      <h2>Order #{order.id}</h2>
-      <p>
-        <strong>Total:</strong> ${order.total.toFixed(2)}
-      </p>
-      <p>
-        <strong>Status:</strong> {order.status}
-      </p>
-      <p>
-        <strong>Created At:</strong>{' '}
-        {new Date(order.created_at).toLocaleString()}
-      </p>
-      <h3>Products:</h3>
-      <ul>
+    <>
+      <h2 className={styles.orderHeader}>Order #{order.id}</h2>
+      <div className={styles.orderInfo}>
+        <p className={styles.orderTotal}>
+          <strong>Total:</strong> ${order.total.toFixed(2)}
+        </p>
+        <p>
+          <strong className={styles.orderStatus}>Status:</strong> {order.status}
+        </p>
+        <p>
+          <strong className={styles.orderCreatedAt}>Created At:</strong>{' '}
+          {new Date(order.created_at).toLocaleString()}
+        </p>
+      </div>
+      <h3 className={styles.productsHeader}>Products:</h3>
+      <ul className={styles.productsList}>
         {order.products.map((product) => (
-          <li key={product.id}>
-            {product.name} - Quantity: {product.quantity}, Price: $
-            {product.regular_price.toFixed(2)}
+          <li
+            className={styles.orderItem}
+            key={product.id}
+          >
+            <span className={styles.productName}>{product.name}</span> -
+            <span className={styles.productQuantity}>
+              {' '}
+              Quantity: {product.quantity}
+            </span>
+            ,
+            <span className={styles.productPrice}>
+              {' '}
+              Price: ${product.regular_price.toFixed(2)}
+            </span>
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 }
