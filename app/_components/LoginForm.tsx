e6c8036c1d6ca78f84fa1c3fa76ecee49712ea6b
NataliaDrause <nataliadrause@gmail.com>
2025-08-02 18:12:11 +0300

    implement server-side auth

--- app/_components/LoginForm.tsx
+++ app/_components/LoginForm.tsx
@@ -1,79 +1,42 @@
 'use client';
 
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+import { login } from '../actions';
 import styles from './login.module.scss';
-import { login } from '../_api/auth-service';
-
-type LoginFormInputs = {
-  email: string;
-  password: string;
-};
 
 function LoginForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginFormInputs>();
-
-  const [loading, setLoading] = useState(false);
-  const [authError, setAuthError] = useState<string | null>(null);
-  const router = useRouter();
-
-  const onSubmit = async (data: LoginFormInputs) => {
-    setLoading(true);
-    setAuthError(null);
-
-    try {
-      await login(data.email, data.password);
-      router.push('/pos');
-    } catch (err) {
-      setAuthError((err as Error).message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
+  const searchParams = useSearchParams();
+  const error = searchParams.get('error');
   return (
     <div className={styles.loginForm}>
       <h2>Log in to your account</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
+
+      <form>
         <div className={styles.inputGroup}>
           <label htmlFor='email'>Email</label>
           <input
             type='email'
+            name='email'
             id='email'
-            className={errors.email ? styles.error : ''}
-            {...register('email', { required: 'Email is required' })}
+            required
           />
-          {errors.email && (
-            <p className={styles.error}>{errors.email.message}</p>
-          )}
         </div>
 
         <div className={styles.inputGroup}>
           <label htmlFor='password'>Password</label>
           <input
             type='password'
+            name='password'
             id='password'
-            className={errors.password ? styles.error : ''}
-            {...register('password', { required: 'Password is required' })}
+            required
           />
-          {errors.password && (
-            <p className={styles.error}>{errors.password.message}</p>
-          )}
         </div>
-
-        {authError && <p className={styles.error}>{authError}</p>}
-
+        {error && <p className={styles.error}>{error}</p>}
         <button
-          type='submit'
-          disabled={loading}
+          formAction={login}
           className={styles.submitButton}
         >
-          {loading ? 'Logging in...' : 'Log in'}
+          Log in
         </button>
       </form>
     </div>
